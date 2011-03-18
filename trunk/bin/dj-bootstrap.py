a2172b1a10c35e74d14450177ca41ccb915488bf
anthony <anthony@17e718c3-2048-44f4-97b5-61abaf20b37d>
2011-03-18 00:47:01 +0000

    move to trunk dir
    git-svn-id: svn://localhost/cuttingEdge/django-skel@3407 17e718c3-2048-44f4-97b5-61abaf20b37d

---
+++ trunk/bin/dj-bootstrap.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""
+setup python environment for ams.
+requires virtualenv and pip to be installed.
+
+"""
+
+import os
+import subprocess
+import shutil
+
+
+pwd = os.path.dirname(__file__)
+
+vedir = os.path.join(pwd,"ve")
+
+if os.path.exists(vedir):
+    shutil.rmtree(vedir)
+
+subprocess.call(["pip","install",
+                    "-E",vedir,
+                    "--ignore-installed",
+                    "--requirement",os.path.join(pwd,"../requirements.txt")])

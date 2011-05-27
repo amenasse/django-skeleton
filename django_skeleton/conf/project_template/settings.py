8676ec92f90a05bc638de011c5ef1f268c2e6215
Anthony Menasse <amenasse@gmail.com>
2011-05-27 17:45:11 +1000

    added django-debug-toolbar configuration to settings

--- django_skeleton/conf/project_template/settings.py
+++ django_skeleton/conf/project_template/settings.py
@@ -104,5 +104,22 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.admin',
     'django_extensions',
+    'debug_toolbar',
 
+)
+
+# django debug toolbar configuration
+DEBUG_TOOLBAR_CONFIG = {}
+DEBUG_TOOLBAR_CONFIG['INTERCEPT_REDIRECTS'] = False
+
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
 )

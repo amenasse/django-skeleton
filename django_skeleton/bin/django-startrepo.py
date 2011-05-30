89fd9aaea0efd2d1fa8b96fb9dbced91f68f052e
Anthony Menasse <amenasse@gmail.com>
2011-05-30 15:27:02 +1000

    allow django project name to differ from the repository name.

--- django_skeleton/bin/django-startrepo.py
+++ django_skeleton/bin/django-startrepo.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python 
 
 import sys
+from optparse import OptionParser
 
 def main(args):
 
+    parser = OptionParser(usage="\n\tcreate a new django based project repository\n\n%prog [options] [REPO NAME]..",version="%prog %(VERSION)")
+
+    parser.add_option("-p","--project",
+                      dest="project_name",default=None,
+                      help="name for django project to use in the repository. Defaults to REPO_NAME")
+
+
+    (options, args) = parser.parse_args()
+
     from django_skeleton.fabfile import django_repo_init
-    project_name = args[1]
-    django_repo_init(project_name)
+
+    repo_name = args[0]
+    project_name = options.project_name or repo_name
+    django_repo_init(repo_name,project_name)
 
 
 if __name__ == '__main__':
--- django_skeleton/fabfile.py
+++ django_skeleton/fabfile.py
@@ -3,13 +3,13 @@ from fabric.api import *
 from fabric.contrib.project import rsync_project
 import os
 
-def django_repo_init(project_name):
+def django_repo_init(repo_name,project_name=None):
     """ create the initial git repository in the current directory """
 
     cwd = os.getcwd()
     # create the git repo
-    local('git init %s' % project_name)
-    repo_dir = os.path.join(cwd,project_name)
+    local('git init %s' % repo_name)
+    repo_dir = os.path.join(cwd,repo_name)
 
     with lcd(repo_dir):
         source_dir = os.path.join(repo_dir,'src')
@@ -23,6 +23,10 @@ def django_repo_init(project_name):
         source_template = os.path.realpath(django_skeleton.conf.__path__[0])
         local('rsync -av --exclude project_template %s/ %s' % (source_template,dest))
 
+
+
     from django_skeleton.management import start_project
+    if project_name is None:
+        project_name = repo_name
     os.chdir(dest)
     start_project(project_name)

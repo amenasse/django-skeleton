30435ae62595060b7bc2115bcf8f6941375d28ed
Anthony Menasse <amenasse@gmail.com>
2011-07-08 12:10:46 +1000

    updated TEMPLATE_CONTEXT_PROCESSORS as per new admin install instructions for django ttrunk

--- django_skeleton/conf/project_template/settings.py
+++ django_skeleton/conf/project_template/settings.py
@@ -103,6 +103,7 @@ TEMPLATE_LOADERS = (
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.static',
+    'django.contrib.messages.context_processors.messages',
 )
 
 MIDDLEWARE_CLASSES = (

d75f7219f33f4260f8b95fdded42a1dd28e0fc5d
Anthony Menasse <amenasse@gmail.com>
2011-07-08 12:02:05 +1000

    fixed path for ADMIN_MEDIA_PREFIX

--- django_skeleton/conf/project_template/settings.py
+++ django_skeleton/conf/project_template/settings.py
@@ -88,7 +88,7 @@ STATICFILES_FINDERS = (
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = ''
